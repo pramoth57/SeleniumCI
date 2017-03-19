6f892f69fd5631a835474c67237693d1216f1354
Pramoth Kumar M <Pramothkumar57@gmail.com>
2017-03-19 13:36:39 -0700

    Test

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -1,11 +1,23 @@
 package example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class NewTest {
   @Test
-  public void f() {
+  public void f() throws InterruptedException {
 	  
-	  System.out.println("pramothkumar");
+
+//	  System.out.println("pramothkumar");
+//	  System.out.println("pramoth kumar edit from git");
+	  
+	  System.setProperty("webdriver.gecko.driver","E:\\Eclipse Mars\\CI_Selenium\\src\\test\\resources\\geckodriver.exe" );
+	  WebDriver d = new FirefoxDriver();
+		
+	d.get("https://www.google.co.in/");
+		Thread.sleep(5000);
+		d.quit();
+
   }
 }
